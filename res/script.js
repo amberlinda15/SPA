87e787c22c3ffac684e39040f2eb454a823c93a6
amberlinda <aul123linda@gmail.com>
2022-02-18 03:48:42 +0530

    final commit

--- res/script.js
+++ res/script.js
@@ -2,14 +2,55 @@ const myApp = angular.module("myApp",["ngRoute"]);
 myApp
 .config(function($routeProvider){
     $routeProvider
-    .when("/",{
-        templateUrl:"../index.html"
-    })
     .when("/page1",{
-        templateUrl:"./pages/page1.html"
+        templateUrl:"../res/pages/page1.html",
+        controller:'firstctrl'
     })
     .when("/page2",{
-        templateUrl:"./pages/page2.html"
+        templateUrl:"../res/pages/page2.html",
+        controller:'secondctrl'
     })
-    .otherwise({redirectTo: '/'});
+    .when("/page2/:first/:last",{
+        templateUrl:"../res/pages/page2.html",
+        controller:'secondctrl'
+    })
+    .when("/page3",{
+        templateUrl:"../res/pages/page3.html",
+        controller:'thirdctrl'
+    })
 })
+
+.controller('firstctrl',function($scope,$http){
+    $http({
+        method:"get",
+        url: 'https://amberlinda15.github.io/json-files/data.json'
+    })
+    .then(response => {
+        $scope.teammembers = response.data.teammembers
+    }); 
+})
+.controller('secondctrl',function($scope,$http,$routeParams){
+
+    $http({
+        method:"get",
+        url: 'https://amberlinda15.github.io/json-files/data.json'
+    })
+    .then(response => {
+        $scope.teammembers = response.data.teammembers
+    }); 
+
+    if($routeParams.first && $routeParams.last){
+        $scope.detail = {first:$routeParams.first,last :$routeParams.last};
+    }
+
+
+})
+.controller('thirdctrl',function($scope,$http){
+    $http({
+        method:"get",
+        url: 'https://amberlinda15.github.io/json-files/data.json'
+    })
+    .then(response => {
+        $scope.teammembers = response.data.teammembers
+    }); 
+});
